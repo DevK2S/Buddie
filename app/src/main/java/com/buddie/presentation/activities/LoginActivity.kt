f63e3f45f7ea9091a52374390575fa3da7f6bbbc
saket <saket23022000@gmail.com>
2021-05-16 15:17:11 +0530

    Firebase

---
+++ app/src/main/java/com/buddie/presentation/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.buddie.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.buddie.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
