4a46d2f8a323ff51cd0bbf7c49892f687d7b0b41
krayong <kgourisaria2001@gmail.com>
2021-05-16 15:44:24 +0530

    .gitignore enhancement

--- app/src/main/java/com/buddie/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/buddie/presentation/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package com.buddie.presentation.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.buddie.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
