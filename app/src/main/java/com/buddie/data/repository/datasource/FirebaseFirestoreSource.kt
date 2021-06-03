dc81eba91dc48494de3988f5f1a71d7ade9523a3
saket <saket23022000@gmail.com>
2021-06-04 00:51:58 +0530

    FireBase Firestore

---
+++ app/src/main/java/com/buddie/data/repository/datasource/FirebaseFirestoreSource.kt
@@ -0,0 +1,11 @@
+package com.buddie.data.repository.datasource
+
+import com.buddie.data.model.UserProfile
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+
+interface FirebaseFirestoreSource {
+
+    suspend fun saveUserProfile(userProfile: UserProfile): Task<DocumentSnapshot>
+    suspend fun getUser(): Task<DocumentSnapshot>
+}
