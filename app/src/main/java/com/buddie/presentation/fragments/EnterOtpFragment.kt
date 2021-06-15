9adbd0ece91022571755962e81f92b76a3edb384
Kayvee08 <karanveerkhatrao140@gmail.com>
2021-06-16 02:53:25 +0530

    Login UI OTP Screen Updated

--- app/src/main/java/com/buddie/presentation/fragments/EnterOtpFragment.kt
+++ app/src/main/java/com/buddie/presentation/fragments/EnterOtpFragment.kt
@@ -2,10 +2,15 @@ package com.buddie.presentation.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
@@ -29,13 +34,63 @@ import java.util.concurrent.TimeUnit
 
 class EnterOtpFragment : BaseFragment(), NumberKeyboardListener {
 
+    class EnteredTextWatcher internal constructor(
+        private val currentView: View,
+        private val nextView: View?
+    ) :
+        TextWatcher {
+        override fun afterTextChanged(editable: Editable) {
+            val text = editable.toString()
+            when (currentView.id) {
+                R.id.codeEt1 -> if (text.length == 1) nextView!!.requestFocus()
+                R.id.codeEt2 -> if (text.length == 1) nextView!!.requestFocus()
+                R.id.codeEt3 -> if (text.length == 1) nextView!!.requestFocus()
+                R.id.codeEt4 -> if (text.length == 1) nextView!!.requestFocus()
+                R.id.codeEt5 -> if (text.length == 1) nextView!!.requestFocus()
+            }
+        }
+
+        override fun beforeTextChanged(
+            arg0: CharSequence,
+            start: Int,
+            count: Int,
+            after: Int
+        ) {
+        }
+
+        override fun onTextChanged(
+            arg0: CharSequence,
+            start: Int,
+            before: Int,
+            count: Int
+        ) {
+        }
+
+    }
+
+    class DeleteTextWatcher internal constructor(
+        private val currentView: EditText,
+        private val previousView: EditText?
+    ) : View.OnKeyListener {
+        override fun onKey(p0: View?, keyCode: Int, event: KeyEvent?): Boolean {
+            if (event!!.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL && currentView.id != R.id.codeEt1 && currentView.text.isEmpty()) {
+                previousView!!.text = null
+                previousView.requestFocus()
+                return true
+            }
+            return false
+        }
+
+
+    }
+
     private lateinit var binding: FragmentEnterOtpBinding
 
     private lateinit var verificationId: String
     private lateinit var forceResendingToken: PhoneAuthProvider.ForceResendingToken
     private lateinit var phoneAuthCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
-    private var amount: Int = 0
+    private var count:Int = 0
     private var st: String = ""
 
     private val loginViewModel: LoginViewModel by activityViewModels()
@@ -63,10 +118,37 @@ class EnterOtpFragment : BaseFragment(), NumberKeyboardListener {
         binding.dispNum.text =
             "Please Enter the OTP we've sent on ${loginViewModel.phoneNumber.value}"
 
+        binding.codeEt1.addTextChangedListener(EnteredTextWatcher(binding.codeEt1, binding.codeEt2))
+        binding.codeEt2.addTextChangedListener(EnteredTextWatcher(binding.codeEt2, binding.codeEt3))
+        binding.codeEt3.addTextChangedListener(EnteredTextWatcher(binding.codeEt3, binding.codeEt4))
+        binding.codeEt4.addTextChangedListener(EnteredTextWatcher(binding.codeEt4, binding.codeEt5))
+        binding.codeEt5.addTextChangedListener(EnteredTextWatcher(binding.codeEt5, binding.codeEt6))
+        binding.codeEt6.addTextChangedListener(EnteredTextWatcher(binding.codeEt6, null))
+
+        binding.codeEt1.setOnKeyListener(DeleteTextWatcher(binding.codeEt1, null))
+        binding.codeEt2.setOnKeyListener(DeleteTextWatcher(binding.codeEt2, binding.codeEt1))
+        binding.codeEt3.setOnKeyListener(DeleteTextWatcher(binding.codeEt3, binding.codeEt2))
+        binding.codeEt4.setOnKeyListener(DeleteTextWatcher(binding.codeEt4, binding.codeEt3))
+        binding.codeEt5.setOnKeyListener(DeleteTextWatcher(binding.codeEt5, binding.codeEt4))
+        binding.codeEt6.setOnKeyListener(DeleteTextWatcher(binding.codeEt6, binding.codeEt5))
+
+        binding.codeEt1.requestFocus()
+        count=1
+
         loginViewModel.otp.observe(viewLifecycleOwner, { otp ->
-            val codeEt = binding.codeEt.text
-            if (codeEt.isNullOrBlank() || codeEt.toString() != otp) {
-                binding.codeEt.setText(otp)
+            //Needs change I replaced codeEt with codeEt1
+            val codeEt = binding.codeEt1.text.toString().trim() + binding.codeEt2.text.toString()
+                .trim() + binding.codeEt3.text.toString().trim() + binding.codeEt4.text.toString()
+                .trim() + binding.codeEt5.text.toString().trim() + binding.codeEt6.text.toString()
+                .trim()
+            if ((codeEt.isNullOrBlank() || codeEt != otp) && !otp.isNullOrEmpty() ) {
+                binding.codeEt1.setText(otp[0].toString())
+                binding.codeEt2.setText(otp[1].toString())
+                binding.codeEt3.setText(otp[2].toString())
+                binding.codeEt4.setText(otp[3].toString())
+                binding.codeEt5.setText(otp[4].toString())
+                binding.codeEt6.setText(otp[5].toString())
+                count=6
             }
         })
     }
@@ -122,7 +204,11 @@ class EnterOtpFragment : BaseFragment(), NumberKeyboardListener {
         binding.codesubmitBtn.setOnClickListener {
             loadingDialog.show()
 
-            val vCode = binding.codeEt.text.toString().trim()
+            //I made a change from codeEt to codeEt1, which is now corrected.
+            val vCode = binding.codeEt1.text.toString().trim() + binding.codeEt2.text.toString()
+                .trim() + binding.codeEt3.text.toString().trim() + binding.codeEt4.text.toString()
+                .trim() + binding.codeEt5.text.toString().trim() + binding.codeEt6.text.toString()
+                .trim()
             loginViewModel.setOtp(vCode)
 
             loginViewModel.otp.observeOnce(viewLifecycleOwner, { otp ->
@@ -253,22 +339,83 @@ class EnterOtpFragment : BaseFragment(), NumberKeyboardListener {
 
     override fun onNumberClicked(number: Int) {
         var s = number.toString()
-        if(st.length<6){
-            st += s
-            binding.codeEt.setText(st)
+        when(count){
+            1-> {
+                binding.codeEt1.setText(s)
+                count++
+                binding.codeEt2.requestFocus()
+            }
+            2-> {
+                binding.codeEt2.setText(s)
+                count++
+                binding.codeEt3.requestFocus()
+            }
+            3-> {
+                binding.codeEt3.setText(s)
+                count++
+                binding.codeEt4.requestFocus()
+            }
+            4-> {
+                binding.codeEt4.setText(s)
+                count++
+                binding.codeEt5.requestFocus()
+            }
+            5-> {
+                binding.codeEt5.setText(s)
+                count++
+                binding.codeEt6.requestFocus()
+            }
+            6-> {
+                binding.codeEt6.setText(s)
+            }
         }
+//        var s = number.toString()
+//        if(st.length<6){
+//            st += s
+//            binding.codeEt.setText(st)
+//        }
     }
 
     override fun onRightAuxButtonClicked() {
-        if (binding.codeEt.text.toString() != "XXXXXX") {
-            st = binding.codeEt.text.toString()
+        when(count){
+            1-> {
+                binding.codeEt1.setText("")
+            }
+            2-> {
+                binding.codeEt2.setText("")
+                count--
+                binding.codeEt1.requestFocus()
+            }
+            3-> {
+                binding.codeEt3.setText("")
+                count--
+                binding.codeEt2.requestFocus()
+            }
+            4-> {
+                binding.codeEt4.setText("")
+                count--
+                binding.codeEt3.requestFocus()
+            }
+            5-> {
+                binding.codeEt5.setText("")
+                count--
+                binding.codeEt4.requestFocus()
+            }
+            6-> {
+                binding.codeEt6.setText("")
+                count--
+                binding.codeEt5.requestFocus()
+            }
         }
-        st = st.dropLast(1)
-        binding.codeEt.setText("XXXXXX")
-        if (st == "") {
-            binding.codeEt.setText("XXXXXX")
-        } else {
-            binding.codeEt.setText(st)
-        }
+//        if (binding.codeEt.text.toString() != "XXXXXX") {
+//            st = binding.codeEt.text.toString()
+//        }
+//        st = st.dropLast(1)
+//        binding.codeEt.setText("XXXXXX")
+//        if (st == "") {
+//            binding.codeEt.setText("XXXXXX")
+//        } else {
+//            binding.codeEt.setText(st)
+//        }
     }
 }
